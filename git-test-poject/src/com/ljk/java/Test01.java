2f70044f3da2b6578ec2faafa1857bd613738a86
xiaoli <377657742@qq.com>
2019-08-12 19:39:00 +0800

    吧本地工程添加到git

---
+++ git-test-poject/src/com/ljk/java/Test01.java
@@ -0,0 +1,12 @@
+package com.ljk.java;
+
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111");
+        System.out.println("1111111111111111111");
+        System.out.println("1111111111111111111");
+        System.out.println("1111111111111111111");System.out.println("1111111111111111111");
+        System.out.println("1111111111111111111");
+
+    }
+}
